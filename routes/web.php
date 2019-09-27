946df783e3b4773a62f74761739cd7c3265a2853
Lucas Assis <lucasassis@MugiwaraNoLuffy.local>
2019-09-27 17:00:35 -0300

    add authentication routes to user and application to its correspondent prefixes

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,26 @@ $router->get('/', function () use ($router) {
 // API route group
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
 
-    $router->post('register', 'AuthController@register');
-    $router->post('login', 'AuthController@login');
-    $router->post('logout', 'AuthController@logout');
-    $router->post('refresh', 'AuthController@refresh');
-
     $router->group(['prefix' => 'users'], function () use ($router) {
         $router->get('/', 'UserController@users');
         $router->get('/me', 'UserController@me');
         $router->get('/{id}', 'UserController@user');
+
+        $router->post('register', 'AuthController@register');
+        $router->post('login', 'AuthController@login');
+        $router->post('logout', 'AuthController@logout');
+        $router->post('refresh', 'AuthController@refresh');
     });
 
     $router->group(['prefix' => 'applications'], function () use ($router) {
         $router->get('/', 'ApplicationController@applications');
         $router->get('/current', 'ApplicationController@current');
         $router->get('/{id}', 'ApplicationController@application');
+
+        $router->post('register', 'AuthApplicationController@register');
+        $router->post('login', 'AuthApplicationController@login');
+        $router->post('logout', 'AuthApplicationController@logout');
+        $router->post('refresh', 'AuthApplicationController@refresh');
     });
 
  });
