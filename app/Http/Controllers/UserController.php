bef7f968b3909f59855fc604a731004bd608697c
Lucas Assis <lucasassis@MugiwaraNoLuffy.local>
2019-09-27 17:04:19 -0300

    add guard api to authentication methods for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:api');
     }
 
     /**
