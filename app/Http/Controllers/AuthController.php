31408d1fb1406bf70c76ddb3e8113eacb1c49440
Lucas Assis <lucasassis@MugiwaraNoLuffy.local>
2019-10-02 17:51:08 -0300

    remove dd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,7 +71,6 @@ class AuthController extends Controller
             return response()->json(['message' => 'Successfully logged out']);
 
         } catch (\Exception $e) {
-            dd($e);
             return response()->json(['error' => 'User Logout Failed!'], 400);
         }
 
