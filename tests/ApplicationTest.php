0ce39070a6397bc26268cc9e520a16161bf4d171
Lucas Assis <lucasassis@MugiwaraNoLuffy.local>
2019-10-03 09:02:10 -0300

    add partial tests

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,246 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+use App\Business\Application;
+
+class ApplicationTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * Test Register Application Function
+     *
+     * @return void
+     *
+     */
+    public function testApplicationRegistration()
+    {
+        $application = Application::register('Test Application', 'client_id', 'client_secret');
+        $this->assertEquals('Test Application', $application->name);
+        $this->assertEquals('client_id', $application->client_id);
+    }
+
+    /**
+     * Test Register Application Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testRegisterApplicationEndpoint()
+    {
+        $bodyData = [
+            'name' => 'Test Application',
+            'client_id' => 'client_id',
+            'client_secret' => 'client_secret',
+            'client_secret_confirmation' => 'client_secret',
+        ];
+        $headers = [];
+        $this->post('api/v1/applications/register', $bodyData, $headers);
+
+        $this->seeStatusCode(201);
+
+        $this->seeJsonStructure(
+            ['application' =>
+                [
+                    'name',
+                    'client_id',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Test Login Application Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testLoginApplicationEndpoint()
+    {
+        Application::register('Test Application', 'client_id', 'client_secret');
+
+        $bodyData = [
+            'client_id' => 'client_id',
+            'client_secret' => 'client_secret'
+        ];
+        $headers = [];
+        $this->post('api/v1/applications/login', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+
+        $this->seeJsonStructure(
+            [
+                'access_token',
+                'token_type',
+                'expires_in'
+            ]
+        );
+    }
+
+    /**
+     * Test Get all applications Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetAllApplicationsEndpoint()
+    {
+
+        $application = Application::register('Test Application', 'client_id', 'client_secret');
+        $access_token = Application::login($application->client_id, 'client_secret');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/applications/', $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['applications' =>
+                [
+                    'name',
+                    'client_id',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Token's Application Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetTokensApplicationEndpoint()
+    {
+        $application = Application::register('Test Application', 'client_id', 'client_secret');
+        $access_token = Application::login($application->client_id, 'client_secret');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/applications/me', $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['application' =>
+                [
+                    'name',
+                    'client_id',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Get Application By Id Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetApplicationByIdEndpoint()
+    {
+        $application = Application::register('Test Application', 'client_id', 'client_secret');
+        $access_token = Application::login($application->client_id, 'client_secret');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/applications/1', $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['application' =>
+                [
+                    'name',
+                    'client_id',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Refresh Application Token Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testRefreshApplicationTokenEndpoint()
+    {
+        $application = Application::register('Test Application', 'client_id', 'client_secret');
+        $access_token = Application::login($application->client_id, 'client_secret');
+
+        $bodyData = [];
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->post('api/v1/applications/refresh', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' =>
+                [
+                    'product_name',
+                    'product_description',
+                    'created_at',
+                    'updated_at',
+                    'links'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Test Logout Application Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testLogoutApplicationEndpoint()
+    {
+        $bodyData = [];
+        $headers = [];
+        $this->post('api/v1/applications/logout', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' =>
+                [
+                    'product_name',
+                    'product_description',
+                    'created_at',
+                    'updated_at',
+                    'links'
+                ]
+            ]
+        );
+    }
+
+
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,247 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+use App\Business\User;
+
+class UserTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * Test Register User Function
+     *
+     * @return void
+     *
+     */
+    public function testUserRegistration()
+    {
+        $user = User::register('Test', 'test@test.com', 'test');
+        $this->assertEquals('Test', $user->name);
+        $this->assertEquals('test@test.com', $user->email);
+    }
+
+    /**
+     * Test Register User Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testRegisterUserEndpoint()
+    {
+        $bodyData = [
+            'name' => 'Test',
+            'email' => 'test@test.com',
+            'password' => 'test',
+            'password_confirmation' => 'test',
+        ];
+        $headers = [];
+        $this->post('api/v1/users/register', $bodyData, $headers);
+
+        $this->seeStatusCode(201);
+
+        $this->seeJsonStructure(
+            ['user' =>
+                [
+                    'name',
+                    'email',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Test Login User Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testLoginUserEndpoint()
+    {
+        User::register('Test', 'test@test.com', 'test');
+
+        $bodyData = [
+            'email' => 'test@test.com',
+            'password' => 'test'
+        ];
+        $headers = [];
+        $this->post('api/v1/users/login', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+
+        $this->seeJsonStructure(
+            [
+                'access_token',
+                'token_type',
+                'expires_in'
+            ]
+        );
+    }
+
+    /**
+     * Test Get all users Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetAllUsersEndpoint()
+    {
+
+        $user = User::register('Test', 'test@test.com', 'test');
+        $access_token = User::login($user->email, 'test');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/users/', $headers);
+
+        $this->seeStatusCode(200);
+        dd($this->response);
+        $this->seeJsonStructure(
+            ['users' =>
+                [
+                    'name',
+                    'email',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Token's User Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetTokensUserEndpoint()
+    {
+        $user = User::register('Test', 'test@test.com', 'test');
+        $access_token = User::login($user->email, 'test');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/users/me', $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['user' =>
+                [
+                    'name',
+                    'email',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Get User By Id Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testGetUserByIdEndpoint()
+    {
+        $user = User::register('Test', 'test@test.com', 'test');
+        $access_token = User::login($user->email, 'test');
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->get('api/v1/users/1', $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['user' =>
+                [
+                    'name',
+                    'email',
+                    'uuid',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ]
+            ]
+        );
+    }
+
+
+    /**
+     * Test Refresh User Token Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testRefreshUserTokenEndpoint()
+    {
+        $user = User::register('Test', 'test@test.com', 'test');
+        $access_token = User::login($user->email, 'test');
+
+        $bodyData = [];
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => "Bearer {$access_token}"
+        ];
+        $this->post('api/v1/users/refresh', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' =>
+                [
+                    'product_name',
+                    'product_description',
+                    'created_at',
+                    'updated_at',
+                    'links'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Test Logout User Endpoint
+     *
+     * @return void
+     *
+     */
+    public function testLogoutUserEndpoint()
+    {
+        $bodyData = [];
+        $headers = [];
+        $this->post('api/v1/users/logout', $bodyData, $headers);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' =>
+                [
+                    'product_name',
+                    'product_description',
+                    'created_at',
+                    'updated_at',
+                    'links'
+                ]
+            ]
+        );
+    }
+
+
+}
